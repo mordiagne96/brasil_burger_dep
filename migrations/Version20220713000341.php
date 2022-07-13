3d332ec0b3e6bce6a42983eec72c2de6931399b3
mordiagne96 <mordiagne1960@gmail.com>
2022-07-13 00:05:16 +0000

    dep

--- migrations/Version20220713000341.php
+++ migrations/Version20220713000341.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220712234818 extends AbstractMigration
+final class Version20220713000341 extends AbstractMigration
 {
     public function getDescription(): string
     {
